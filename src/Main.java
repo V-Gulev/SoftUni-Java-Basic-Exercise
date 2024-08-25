b3a2fbaf4da8dc2db0e32c217f4c7140c416321b
V-Gulev <velizargulev@abv.bg>
2024-08-25 20:28:09 +0300

    Cat Food Calculator

--- src/Main.java
+++ src/Main.java
@@ -3,45 +3,28 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String type = scanner.nextLine();
-        int amount = Integer.parseInt(scanner.nextLine());
-        int day = Integer.parseInt(scanner.nextLine());
-        double pricePerCake = 0;
-        switch (type) {
-            case "Cake":
-                if (day <= 15) {
-                    pricePerCake = 24;
-                } else  {
-                    pricePerCake = 28.70;
-                }
-                break;
-            case "Souffle":
-                if (day <= 15) {
-                    pricePerCake = 6.66;
-                } else  {
-                    pricePerCake = 9.80;
-                }
-                break;
-            case "Baklava":
-                if (day <= 15) {
-                    pricePerCake = 12.60;
-                } else  {
-                    pricePerCake = 16.98;
-                }
-                break;
-            default:
-                break;
-        }
-        double totalPrice = pricePerCake * amount;
-        if (totalPrice >= 100 && totalPrice < 200 && day <= 22) {
-            totalPrice = totalPrice * 0.85;
-        } else if (totalPrice >= 200 && day <= 22) {
-            totalPrice = totalPrice * 0.75;
+        int cats = Integer.parseInt(scanner.nextLine());
+        int smallCats = 0;
+        int mediumCats = 0;
+        int bigCats = 0;
+        double foodEaten = 0;
+        for (int i = 0; i < cats; i++) {
+            int cat = Integer.parseInt(scanner.nextLine());
+
+            if (cat >= 100 && cat < 200) {
+                smallCats++;
+            } else if (cat >= 200 && cat < 300) {
+                mediumCats++;
+            } else if (cat >= 300 && cat < 400) {
+                bigCats++;
+            }
+
+            foodEaten += cat;
         }
 
-        if (day <= 15) {
-            totalPrice = totalPrice * 0.90;
-        }
-        System.out.printf("%.2f",totalPrice);
+        foodEaten = foodEaten / 1000;
+        double neededMoney = foodEaten * 12.45;
+        System.out.printf("Group 1: %d cats.%nGroup 2: %d cats.%nGroup 3: %d cats.%nPrice for food per day: %.2f lv.",smallCats,mediumCats,bigCats,neededMoney);
+
     }
 }
