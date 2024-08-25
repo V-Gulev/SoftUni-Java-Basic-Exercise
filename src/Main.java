642c6107b1377ea63c79cf9a4e26361d280d0773
V-Gulev <velizargulev@abv.bg>
2024-08-25 20:27:17 +0300

    Pastry

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,45 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int cpus = Integer.parseInt(scanner.nextLine());
-        int workers = Integer.parseInt(scanner.nextLine());
-        int days = Integer.parseInt(scanner.nextLine());
-
-        int hoursWorked = workers * days * 8;
-        int cpusMade = (int) Math.floor(hoursWorked / 3);
-
-        if (cpusMade >= cpus) {
-            double profit = (cpusMade - cpus) * 110.10;
-            System.out.printf("Profit: -> %.2f BGN",profit);
-        } else {
-            double loss = (cpus - cpusMade) * 110.10;
-            System.out.printf("Losses: -> %.2f BGN",loss);
+        String type = scanner.nextLine();
+        int amount = Integer.parseInt(scanner.nextLine());
+        int day = Integer.parseInt(scanner.nextLine());
+        double pricePerCake = 0;
+        switch (type) {
+            case "Cake":
+                if (day <= 15) {
+                    pricePerCake = 24;
+                } else  {
+                    pricePerCake = 28.70;
+                }
+                break;
+            case "Souffle":
+                if (day <= 15) {
+                    pricePerCake = 6.66;
+                } else  {
+                    pricePerCake = 9.80;
+                }
+                break;
+            case "Baklava":
+                if (day <= 15) {
+                    pricePerCake = 12.60;
+                } else  {
+                    pricePerCake = 16.98;
+                }
+                break;
+            default:
+                break;
         }
+        double totalPrice = pricePerCake * amount;
+        if (totalPrice >= 100 && totalPrice < 200 && day <= 22) {
+            totalPrice = totalPrice * 0.85;
+        } else if (totalPrice >= 200 && day <= 22) {
+            totalPrice = totalPrice * 0.75;
+        }
+
+        if (day <= 15) {
+            totalPrice = totalPrice * 0.90;
+        }
+        System.out.printf("%.2f",totalPrice);
     }
 }
