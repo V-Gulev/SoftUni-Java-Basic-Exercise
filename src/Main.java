bd099bf5312f9b7d99daa69af455224e5e21d47b
V-Gulev <velizargulev@abv.bg>
2024-08-25 20:25:36 +0300

    Painting a Room

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,22 @@
+import java.util.*;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int buckets = Integer.parseInt(scanner.nextLine());
+        int rolls = Integer.parseInt(scanner.nextLine());
+        double priceGloves = Double.parseDouble(scanner.nextLine());
+        double priceBrush = Double.parseDouble(scanner.nextLine());
+
+        double totalBucket = buckets * 21.50;
+        double totalRoll = rolls * 5.20;
+        int gloves = (int) Math.ceil(0.35 * rolls);
+        int brushes = (int) Math.floor(0.48 * buckets);
+        double totalGloves = priceGloves * gloves;
+        double totalBrushes = priceBrush * brushes;
+        double total = totalBrushes + totalGloves + totalBucket + totalRoll;
+        double delivery = total / 15;
+        System.out.printf("This delivery will cost %.2f lv.",delivery);
+
+    }
 }
