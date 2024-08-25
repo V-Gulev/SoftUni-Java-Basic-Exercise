dc46309ca5d3900808aad8aacedfd67c18e5154e
V-Gulev <velizargulev@abv.bg>
2024-08-25 20:28:49 +0300

    Dog Care

--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,29 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int cats = Integer.parseInt(scanner.nextLine());
-        int smallCats = 0;
-        int mediumCats = 0;
-        int bigCats = 0;
-        double foodEaten = 0;
-        for (int i = 0; i < cats; i++) {
-            int cat = Integer.parseInt(scanner.nextLine());
+        int foodKG = Integer.parseInt(scanner.nextLine());
+        foodKG = foodKG * 1000;
+        int foodPerDay;
+        int totalFoodEaten = 0;
+        while (true) {
+            String input = scanner.nextLine();
 
-            if (cat >= 100 && cat < 200) {
-                smallCats++;
-            } else if (cat >= 200 && cat < 300) {
-                mediumCats++;
-            } else if (cat >= 300 && cat < 400) {
-                bigCats++;
+            if (input.equals("Adopted")) {
+                break;
+            } else {
+                foodPerDay = Integer.parseInt(input);
             }
 
-            foodEaten += cat;
+            totalFoodEaten += foodPerDay;
         }
 
-        foodEaten = foodEaten / 1000;
-        double neededMoney = foodEaten * 12.45;
-        System.out.printf("Group 1: %d cats.%nGroup 2: %d cats.%nGroup 3: %d cats.%nPrice for food per day: %.2f lv.",smallCats,mediumCats,bigCats,neededMoney);
+        if (totalFoodEaten > foodKG) {
+            int neededFood = totalFoodEaten - foodKG;
+            System.out.printf("Food is not enough. You need %d grams more.",neededFood);
+        } else {
+            int leftFood = foodKG - totalFoodEaten;
+            System.out.printf("Food is enough! Leftovers: %d grams.",leftFood);
+        }
 
     }
 }
